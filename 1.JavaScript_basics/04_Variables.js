f5bcbaefd487bf90f2d6a3402e0a6f85c611d0c2
aston4ever <aston4ever@gmail.com>
2022-10-30 11:01:45 +0300

    feat: complete tasks chapter 4

---
+++ 1.JavaScript_basics/04_Variables.js
@@ -0,0 +1,18 @@
+(function (){
+    let admin, name;
+    name = 'John';
+    admin = name;
+    alert(admin);
+})();
+
+(function (){
+    let ourPlanetEarth;
+    let currentUserName;
+})();
+
+(function (){
+    const BIRTHDAY = '18.04.1982';
+    const age = someCode(birthday);
+})();
+
+
