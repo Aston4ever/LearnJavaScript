c025f033d871db2391bf832533a87c7f7e3d84e8
aston4ever <aston4ever@gmail.com>
2022-11-11 00:00:04 +0300

    feat: finish task 2

--- 2.objects_basics/01_objects.js
+++ 2.objects_basics/01_objects.js
@@ -1,25 +1,31 @@
-(function(){
-        /*Создайте пустой объект user.
-        Добавьте свойство name со значением John.
-        Добавьте свойство surname со значением Smith.
-        Измените значение свойства name на Pete.
-        Удалите свойство name из объекта.*/
-let user = {
-    name: "John",
-    surname: "Smith",
-}
-user.name = "Pete";
-delete user.name;
+(function () {
+    /*Создайте пустой объект user.
+    Добавьте свойство name со значением John.
+    Добавьте свойство surname со значением Smith.
+    Измените значение свойства name на Pete.
+    Удалите свойство name из объекта.*/
+    let user = {
+        name: "John",
+        surname: "Smith",
+    }
+    user.name = "Pete";
+    delete user.name;
 })();
 
-(function(){
+(function () {
+    // Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+    function isEmpty(obj) {
+        for (let key in obj) {
+            return false
+        }
+        return true
+    }
+})();
+
+(function () {
 
 })();
 
-(function(){
-
-})();
-
-(function(){
+(function () {
 
 })();
