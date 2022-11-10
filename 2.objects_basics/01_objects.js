1afbaa47303cc1311fecf1a425f13e53f149b4dd
aston4ever <aston4ever@gmail.com>
2022-11-10 23:48:14 +0300

    feat: finish 1 task

---
+++ 2.objects_basics/01_objects.js
@@ -0,0 +1,25 @@
+(function(){
+        /*Создайте пустой объект user.
+        Добавьте свойство name со значением John.
+        Добавьте свойство surname со значением Smith.
+        Измените значение свойства name на Pete.
+        Удалите свойство name из объекта.*/
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+user.name = "Pete";
+delete user.name;
+})();
+
+(function(){
+
+})();
+
+(function(){
+
+})();
+
+(function(){
+
+})();
