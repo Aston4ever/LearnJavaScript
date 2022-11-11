e2d6b9703488583cd871ac186662111a4f4f5fbe
aston4ever <aston4ever@gmail.com>
2022-11-11 16:12:58 +0300

    feat: finish task 5

--- 2.objects_basics/01_objects.js
+++ 2.objects_basics/01_objects.js
@@ -48,5 +48,13 @@
 })();
 
 (function () {
+    // Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
 
+    function multiplyNumeric(obj) {
+        for (let key in obj) {
+            if (typeof obj[key] === 'number') {
+                obj[key] *= 2
+            }
+        }
+    }
 })();
