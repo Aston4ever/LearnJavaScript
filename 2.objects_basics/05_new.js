c49311217e42eb4c420ccc84f64873bf95c84e20
aston4ever <aston4ever@gmail.com>
2022-11-15 16:30:33 +0300

    feat: complete 2 task

--- 2.objects_basics/05_new.js
+++ 2.objects_basics/05_new.js
@@ -11,5 +11,40 @@ alert( a == b ); // true
 */
 
 let obj = {}
-function A() { return obj }
-function B() { return obj }
+
+function A() {
+    return obj
+}
+
+function B() {
+    return obj
+}
+
+/*
+* Создайте функцию-конструктор Calculator, которая создаёт объекты с тремя методами:
+    read() запрашивает два значения при помощи prompt и сохраняет их значение в свойствах объекта.
+    sum() возвращает сумму этих свойств.
+    mul() возвращает произведение этих свойств.
+Например:
+let calculator = new Calculator();
+calculator.read();
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );*/
+
+function Calculator() {
+    this.input = function () {
+        this.a = +prompt("введите первое число", 0);
+        this.b = +prompt("введите второе число", 0);
+    }
+    this.sum = function () {
+        return this.a + this.b
+    }
+    this.mul = function () {
+        return this.a * this.b
+    }
+}
+
+let two = new Calculator();
+two.input();
+alert(two.sum());
+alert(two.mul())
