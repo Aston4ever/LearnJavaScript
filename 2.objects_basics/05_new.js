ac15eef7f2a995d419061e23b530a52fe6d84d48
aston4ever <aston4ever@gmail.com>
2022-11-15 17:04:28 +0300

    feat: complete 3 task

--- 2.objects_basics/05_new.js
+++ 2.objects_basics/05_new.js
@@ -31,7 +31,7 @@ calculator.read();
 alert( "Sum=" + calculator.sum() );
 alert( "Mul=" + calculator.mul() );*/
 
-function Calculator() {
+/*function Calculator() {
     this.input = function () {
         this.a = +prompt("введите первое число", 0);
         this.b = +prompt("введите второе число", 0);
@@ -47,4 +47,30 @@ function Calculator() {
 let two = new Calculator();
 two.input();
 alert(two.sum());
-alert(two.mul())
+alert(two.mul())*/
+
+/*
+    Создайте функцию-конструктор Accumulator(startingValue).
+    Объект, который она создаёт, должен уметь следующее:
+    Хранить «текущее значение» в свойстве value. Начальное значение устанавливается в аргументе конструктора startingValue.
+    Метод read() должен использовать prompt для считывания нового числа и прибавления его к value.
+    Другими словами, свойство value представляет собой сумму всех введённых пользователем значений, с учётом начального значения startingValue.
+    Ниже вы можете посмотреть работу кода:
+let accumulator = new Accumulator(1); // начальное значение 1
+accumulator.read(); // прибавляет введённое пользователем значение к текущему значению
+accumulator.read(); // прибавляет введённое пользователем значение к текущему значению
+
+alert(accumulator.value); // выведет сумму этих значений*/
+
+function Accumulator(startingValue = 0){
+    this.value = startingValue;
+    this.read = function () {
+        let sumNumber = +prompt('enter number for sum', 0)
+        this.value += sumNumber;
+    }
+}
+let calc = new Accumulator(10);
+calc.read();
+calc.read();
+calc.read();
+alert(calc.value);
